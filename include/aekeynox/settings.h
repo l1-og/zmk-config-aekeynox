16e4c1651d16ee0326c2fa768deebcf6b94a0578
l1-og <183477804+l1-og@users.noreply.github.com>
2026-08-30 19:05:45 +0200

    Update settings.h

--- include/aekeynox/settings.h
+++ include/aekeynox/settings.h
@@ -89,7 +89,7 @@
 // Useless on QWERTY/English keymaps or non-Windows hosts, recommended otherwise.
 // XXX This assumes NumLock is ON. It will not work otherwise.
 
-// #define ENABLE_CP1252_ALT_CODES
+#define ENABLE_CP1252_ALT_CODES
 
 // [Experimental]
 // Uncomment the following line for an improved dead key support.
